9f3c519583937787d28b3978e48ef21c0ee99c88
inst8 <107987020+inst8@users.noreply.github.com>
2023-04-05 00:25:32 +1000

    Add files via upload

---
+++ Encryption/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnEncrypt = New System.Windows.Forms.Button()
+        Me.btnDecrypt = New System.Windows.Forms.Button()
+        Me.dgvInput = New System.Windows.Forms.DataGridView()
+        Me.File = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dgvOutput = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dgvDecrypt = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.tmrCheck = New System.Windows.Forms.Timer(Me.components)
+        Me.btnFolder = New System.Windows.Forms.Button()
+        CType(Me.dgvInput, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvOutput, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvDecrypt, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnEncrypt
+        '
+        Me.btnEncrypt.Location = New System.Drawing.Point(16, 402)
+        Me.btnEncrypt.Name = "btnEncrypt"
+        Me.btnEncrypt.Size = New System.Drawing.Size(89, 36)
+        Me.btnEncrypt.TabIndex = 1
+        Me.btnEncrypt.Text = "Encrypt"
+        Me.btnEncrypt.UseVisualStyleBackColor = True
+        '
+        'btnDecrypt
+        '
+        Me.btnDecrypt.Location = New System.Drawing.Point(111, 402)
+        Me.btnDecrypt.Name = "btnDecrypt"
+        Me.btnDecrypt.Size = New System.Drawing.Size(89, 36)
+        Me.btnDecrypt.TabIndex = 2
+        Me.btnDecrypt.Text = "Decrypt"
+        Me.btnDecrypt.UseVisualStyleBackColor = True
+        '
+        'dgvInput
+        '
+        Me.dgvInput.AllowUserToAddRows = False
+        Me.dgvInput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvInput.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.File})
+        Me.dgvInput.Location = New System.Drawing.Point(12, 49)
+        Me.dgvInput.Name = "dgvInput"
+        Me.dgvInput.RowHeadersWidth = 62
+        Me.dgvInput.RowTemplate.Height = 28
+        Me.dgvInput.Size = New System.Drawing.Size(364, 347)
+        Me.dgvInput.TabIndex = 3
+        '
+        'File
+        '
+        Me.File.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.File.HeaderText = "File"
+        Me.File.MinimumWidth = 8
+        Me.File.Name = "File"
+        Me.File.ReadOnly = True
+        '
+        'dgvOutput
+        '
+        Me.dgvOutput.AllowUserToAddRows = False
+        Me.dgvOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvOutput.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1})
+        Me.dgvOutput.Location = New System.Drawing.Point(424, 49)
+        Me.dgvOutput.Name = "dgvOutput"
+        Me.dgvOutput.RowHeadersWidth = 62
+        Me.dgvOutput.RowTemplate.Height = 28
+        Me.dgvOutput.Size = New System.Drawing.Size(364, 347)
+        Me.dgvOutput.TabIndex = 4
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.DataGridViewTextBoxColumn1.HeaderText = "File"
+        Me.DataGridViewTextBoxColumn1.MinimumWidth = 8
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.ReadOnly = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Input"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(429, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Output"
+        '
+        'dgvDecrypt
+        '
+        Me.dgvDecrypt.AllowUserToAddRows = False
+        Me.dgvDecrypt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDecrypt.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn2})
+        Me.dgvDecrypt.Location = New System.Drawing.Point(841, 49)
+        Me.dgvDecrypt.Name = "dgvDecrypt"
+        Me.dgvDecrypt.RowHeadersWidth = 62
+        Me.dgvDecrypt.RowTemplate.Height = 28
+        Me.dgvDecrypt.Size = New System.Drawing.Size(364, 347)
+        Me.dgvDecrypt.TabIndex = 7
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.DataGridViewTextBoxColumn2.HeaderText = "File"
+        Me.DataGridViewTextBoxColumn2.MinimumWidth = 8
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.ReadOnly = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(837, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Decrypted"
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(953, 402)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(252, 36)
+        Me.btnDelete.TabIndex = 9
+        Me.btnDelete.Text = "Clear Output and Decrypted"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'tmrCheck
+        '
+        '
+        'btnFolder
+        '
+        Me.btnFolder.Location = New System.Drawing.Point(206, 402)
+        Me.btnFolder.Name = "btnFolder"
+        Me.btnFolder.Size = New System.Drawing.Size(89, 36)
+        Me.btnFolder.TabIndex = 10
+        Me.btnFolder.Text = "Folder"
+        Me.btnFolder.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1215, 450)
+        Me.Controls.Add(Me.btnFolder)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.dgvDecrypt)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvOutput)
+        Me.Controls.Add(Me.dgvInput)
+        Me.Controls.Add(Me.btnDecrypt)
+        Me.Controls.Add(Me.btnEncrypt)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvInput, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvOutput, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvDecrypt, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnEncrypt As Button
+    Friend WithEvents btnDecrypt As Button
+    Friend WithEvents dgvInput As DataGridView
+    Friend WithEvents File As DataGridViewTextBoxColumn
+    Friend WithEvents dgvOutput As DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents dgvDecrypt As DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents tmrCheck As Timer
+    Friend WithEvents btnFolder As Button
+End Class
---
+++ Encryption/Form1.vb
@@ -0,0 +1,235 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class Form1
+
+    Private Sub tmrCheck_Tick(sender As Object, e As EventArgs) Handles tmrCheck.Tick
+        'clear the datagridviews
+        dgvInput.Rows.Clear()
+        dgvOutput.Rows.Clear()
+        dgvDecrypt.Rows.Clear()
+        'load the names of the files in the output folder and input and put them into dgvInput and dgvOutput
+        Dim inputFolder As String = "input"
+        Dim outputFolder As String = "output"
+        Dim decryptedFolder As String = "decrypted"
+        'now get the full paths to the folders
+        inputFolder = Path.Combine(Application.StartupPath, inputFolder)
+        outputFolder = Path.Combine(Application.StartupPath, outputFolder)
+        decryptedFolder = Path.Combine(Application.StartupPath, decryptedFolder)
+        'now get the files in the folders
+        Dim inputFiles() As String = Directory.GetFiles(inputFolder)
+        Dim outputFiles() As String = Directory.GetFiles(outputFolder)
+        Dim decryptedFiles() As String = Directory.GetFiles(decryptedFolder)
+        'now add the files to the datagridviews
+        For Each file As String In inputFiles
+            dgvInput.Rows.Add(Path.GetFileName(file))
+        Next
+        For Each file As String In outputFiles
+            dgvOutput.Rows.Add(Path.GetFileName(file))
+        Next
+        For Each file As String In decryptedFiles
+            dgvDecrypt.Rows.Add(Path.GetFileName(file))
+        Next
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'check for input and output folders and create them if they don't exist
+        Dim inputFolder As String = "input"
+        Dim outputFolder As String = "output"
+        Dim decryptedFolder As String = "decrypted"
+        'now get the full paths to the folders
+        inputFolder = Path.Combine(Application.StartupPath, inputFolder)
+        outputFolder = Path.Combine(Application.StartupPath, outputFolder)
+        decryptedFolder = Path.Combine(Application.StartupPath, decryptedFolder)
+        'now check if the folders exist and create them if they don't
+        If Not Directory.Exists(inputFolder) Then
+            Directory.CreateDirectory(inputFolder)
+        End If
+        If Not Directory.Exists(outputFolder) Then
+            Directory.CreateDirectory(outputFolder)
+        End If
+        If Not Directory.Exists(decryptedFolder) Then
+            Directory.CreateDirectory(decryptedFolder)
+        End If
+        'starts the timer
+        tmrCheck.Interval = 1000
+        tmrCheck.Start()
+    End Sub
+
+    Private Sub btnEncrypt_Click(sender As Object, e As EventArgs) Handles btnEncrypt.Click
+        Dim inputFolder As String = "input"
+        Dim outputFolder As String = "output"
+
+        ' Generate key and IV
+        Dim keyString As String = "KxcaLlHsKi65nqCC"
+        Dim keyBytes() As Byte
+        Using sha256 As New SHA256CryptoServiceProvider()
+            keyBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(keyString))
+        End Using
+        'make iv
+        Dim iv() As Byte = New Byte(15) {}
+        Using rng As New RNGCryptoServiceProvider()
+            rng.GetBytes(iv)
+        End Using
+
+        Dim aes As New AesEncryption(keyBytes)
+        ' Get full paths to input and output folders
+        inputFolder = Path.Combine(Application.StartupPath, inputFolder)
+        outputFolder = Path.Combine(Application.StartupPath, outputFolder)
+        ' Get all files in input folder
+        Dim files() As String = Directory.GetFiles(inputFolder)
+
+        ' Encrypt and output each file
+        For Each file As String In files
+            ' Generate output filename
+            Dim fileName As String = Path.GetFileName(file)
+            Dim outputFile As String = Path.Combine(outputFolder, fileName + ".lastbreath")
+
+            ' Encrypt file and write to output
+            aes.EncryptFile(file, outputFile)
+        Next
+
+        MessageBox.Show("Encryption complete!")
+    End Sub
+
+    Private Sub btnDecrypt_Click(sender As Object, e As EventArgs) Handles btnDecrypt.Click
+        Dim inputFolder As String = "output"
+        Dim outputFolder As String = "decrypted"
+
+        ' Generate key and IV
+        Dim keyString As String = "KxcaLlHsKi65nqCC"
+        Dim keyBytes() As Byte
+        Using sha256 As New SHA256CryptoServiceProvider()
+            keyBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(keyString))
+        End Using
+        'make iv
+        Dim iv() As Byte = New Byte(15) {}
+        Using rng As New RNGCryptoServiceProvider()
+            rng.GetBytes(iv)
+        End Using
+
+        Dim aes As New AesEncryption(keyBytes)
+
+        ' Get full paths to input and output folders
+        inputFolder = Path.Combine(Application.StartupPath, inputFolder)
+        outputFolder = Path.Combine(Application.StartupPath, outputFolder)
+
+        ' Get all files in input folder
+        Dim files() As String = Directory.GetFiles(inputFolder)
+
+        ' Decrypt and output each file
+        For Each file As String In files
+            ' Check if file name contains "lastbreath"
+            If Not file.Contains("lastbreath") Then
+                Continue For ' Skip file if it doesn't contain "lastbreath"
+            End If
+
+            ' Generate output filename
+            Dim fileName As String = Path.GetFileNameWithoutExtension(file)
+            'get the file extension and remove lastbreath from it
+            Dim fileExtension As String = Path.GetExtension(file)
+            'remove the word lastbreath from the file extension
+            fileExtension = fileExtension.Replace("lastbreath", "")
+
+            Dim outputFile As String = Path.Combine(outputFolder, fileName.Replace(".lastbreath", "") & fileExtension)
+            'remove lastbreath from file extension
+            fileExtension = fileExtension.Substring(1)
+            outputFile = Path.Combine(outputFolder, fileName.Replace(".lastbreath", "") & "." & fileExtension)
+            ' Decrypt file and write to output
+            aes.DecryptFile(file, outputFile)
+        Next
+        '
+        MessageBox.Show("Decryption complete!")
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        'delete output folder and decrypted folder
+        Dim output As String = "output"
+        Dim decrypted As String = "decrypted"
+        'get the full paths to the folders
+        output = Path.Combine(Application.StartupPath, output)
+        decrypted = Path.Combine(Application.StartupPath, decrypted)
+        'delete the folders
+
+        System.IO.Directory.Delete(output, True)
+        System.IO.Directory.Delete(decrypted, True)
+        'make new folders
+        System.IO.Directory.CreateDirectory(output)
+        System.IO.Directory.CreateDirectory(decrypted)
+
+    End Sub
+
+    Private Sub btnFolder_Click(sender As Object, e As EventArgs) Handles btnFolder.Click
+        'open where the folders are located
+        Process.Start(Application.StartupPath)
+    End Sub
+
+End Class
+
+Public Class AesEncryption
+
+    Private ReadOnly key As Byte()
+    Private ReadOnly iv As Byte()
+
+    Public Sub New(ByVal key As Byte())
+        Me.key = key
+        Using aes As New AesCryptoServiceProvider()
+            aes.GenerateIV()
+            iv = aes.IV
+        End Using
+    End Sub
+
+    Public Sub EncryptFile(ByVal inputFile As String, ByVal outputFile As String)
+        Using aes As New AesCryptoServiceProvider()
+            aes.Key = key
+            aes.IV = iv
+            Dim encryptor As ICryptoTransform = aes.CreateEncryptor(aes.Key, aes.IV)
+            Using fsIn As New FileStream(inputFile, FileMode.Open, FileAccess.Read)
+                Using fsOut As New FileStream(outputFile, FileMode.Create, FileAccess.Write)
+                    ' Write the IV to the beginning of the encrypted file.
+                    fsOut.Write(iv, 0, iv.Length)
+                    Using cs As New CryptoStream(fsOut, encryptor, CryptoStreamMode.Write)
+                        Dim bufferLength As Integer = 4096
+                        Dim buffer(bufferLength - 1) As Byte
+                        Dim bytesRead As Integer
+                        Do
+                            bytesRead = fsIn.Read(buffer, 0, bufferLength)
+                            If bytesRead > 0 Then
+                                cs.Write(buffer, 0, bytesRead)
+                            End If
+                        Loop While bytesRead > 0
+                    End Using
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Public Sub DecryptFile(ByVal inputFile As String, ByVal outputFile As String)
+        Using aes As New AesCryptoServiceProvider()
+            aes.Key = key
+            Using fsIn As New FileStream(inputFile, FileMode.Open, FileAccess.Read)
+                ' Read the IV from the encrypted file.
+                Dim ivFromEncryptedFile(15) As Byte
+                fsIn.Read(ivFromEncryptedFile, 0, 16)
+                aes.IV = ivFromEncryptedFile
+                Dim decryptor As ICryptoTransform = aes.CreateDecryptor(aes.Key, aes.IV)
+                Using fsOut As New FileStream(outputFile, FileMode.Create, FileAccess.Write)
+                    Using cs As New CryptoStream(fsIn, decryptor, CryptoStreamMode.Read)
+                        Dim bufferLength As Integer = 4096
+                        Dim buffer(bufferLength - 1) As Byte
+                        Dim bytesRead As Integer
+                        Do
+                            bytesRead = cs.Read(buffer, 0, bufferLength)
+                            If bytesRead > 0 Then
+                                fsOut.Write(buffer, 0, bytesRead)
+                            End If
+                        Loop While bytesRead > 0
+                    End Using
+                End Using
+            End Using
+        End Using
+    End Sub
+
+End Class
